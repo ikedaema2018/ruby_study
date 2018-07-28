f2653f413acdd093896fa69c460e5dc701c15065
ikedaema2018 <ikedaema0817>
2018-07-28 13:16:05 +0900

    15,16,17,18,19

--- No_13.rb
+++ No_13.rb
@@ -1,9 +1,1 @@
-require './common'
-
-class CreateUsers < ActiveRecord::Migration[5.0]
-  create_table :users do |t|
-    t.column :gender, :string
-    t.column :age, :string
-    t.timestamps
-  end
-end
+# sequel proをインストール
--- No_14.rb
+++ No_14.rb
@@ -1,14 +1,12 @@
+
+
 require './common'
 
-user = User.new
-user.gender = "男性"
-user.age = 36
-user.save
-
-# __END__
-
-# HeadLine.create(
-#     label: 'EVENT',
-#     title: '【結果速報】GLOBAL GEEK AUDITION "LAB4"開催しました！'
-# )
+class CreateUsers < ActiveRecord::Migration[5.0]
+  create_table :users do |t|
+    t.column :gender, :string
+    t.column :age, :string
+    t.timestamps
+  end
+end
 
